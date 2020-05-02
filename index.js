f837dc98268171c3267a85b056af9b7c08bda1df
Victor Nery <victor.teixeira@zflow.com.br>
2020-05-02 04:06:39 -0300

    First commit 🤩

---
+++ index.js
@@ -0,0 +1,71 @@
+/**
+ * @name CrawlerReclameAqui
+ * @file index.js
+ * @description A intenção desse crawler é pegar todos os resultados de uma determinada
+ * página do Reclame Aqui com seus determinados títulos, conteúdos e metadados. Esse
+ * crawler gerará um JSON como o mesmo localizado no arquivo `example.json`
+ * @summary O site do Reclame Aqui, nesta data, foi criado com Angular. Precisamos ter
+ * atenção com as classes que nosso Crawler/Scrapper irá pegar, pois elas podem ser variáveis
+ * em relação a página que iremos começar a fazer o crawler.
+ * @author Victor Nery <victornery.t@gmail.com>
+ * 
+ * A página base que utilizamos para esse exemplo: 
+ * - https://www.reclameaqui.com.br/empresa/estacio/lista-reclamacoes/?produto=0000000000001136
+ */
+
+const puppeteer = require('puppeteer');
+const fs = require('fs');
+
+const { log } = console;
+
+/**
+ * @var CRAWLER_DATA
+ * @description Mantém e centraliza as informações do JSON externo gerado e informações de como
+ * o mesmo será populado.
+ */
+
+const CRAWLER_DATA = {
+  title: 'Estácio',
+  from: 'Reclame Aqui',
+  link: 'https://www.reclameaqui.com.br/empresa/estacio/lista-reclamacoes/?produto=0000000000001136',
+  date: new Date().toLocaleDateString('pt-BR'),
+};
+
+/**
+ * @var OPTIONS
+ * @description Organiza e centraliza tudo que obtermos de opções para parametrizações do nosso
+ * puppeteer.
+ */
+
+const OPTIONS = {
+  application: {
+    isDebuggerActived: true,
+  },
+  puppeteer: {
+    headless: false,
+  },
+};
+
+const { application: isDebuggerActived } = OPTIONS
+
+isDebuggerActived && log('Iniciando aplicação... ');
+
+(async () => {
+  const browser = await puppeteer.launch(OPTIONS.puppeteer);
+  isDebuggerActived && log('Iniciando Browser');
+
+  const page = await browser.newPage();
+  isDebuggerActived && log('Abrindo nova página');
+  
+  try {
+    await page.goto(CRAWLER_DATA.link);
+    browser.close();
+  } catch (err) {
+    console.error(`Algum problema aconteceu! 😥`, err);
+    browser.close();
+  }
+
+})();
+
+// const title = document.querySelector('h1.ng-binding');
+// const content = document.querySelector('.complain-body p');
